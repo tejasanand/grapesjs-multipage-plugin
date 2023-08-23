6d5793889146a4abeece3e48cedcb4493e113436
alakhpc <36898190+alakhpc@users.noreply.github.com>
2023-08-22 20:27:03 -0400

    do stuff

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,15 +1,15 @@
-import type { Editor } from 'grapesjs';
-import { RequiredPluginOptions } from '..';
+import type { Editor } from "grapesjs";
+import { RequiredPluginOptions } from "..";
 import {
   cmdClear,
   cmdDeviceDesktop,
   cmdDeviceMobile,
   cmdDeviceTablet,
   pageShit,
-} from './../consts';
-import openImport from './openImport';
+} from "./../consts";
+import openImport from "./openImport";
 
-import { CommandObject } from 'grapesjs';
+import { CommandObject } from "grapesjs";
 
 export default (editor: Editor, config: RequiredPluginOptions) => {
   const { Commands } = editor;
@@ -18,28 +18,48 @@ export default (editor: Editor, config: RequiredPluginOptions) => {
   openImport(editor, config);
 
   Commands.add(cmdDeviceDesktop, {
-    run: ed => ed.setDevice('Desktop'),
-    stop: () => { },
+    run: (ed) => ed.setDevice("Desktop"),
+    stop: () => {},
   });
   Commands.add(cmdDeviceTablet, {
-    run: ed => ed.setDevice('Tablet'),
-    stop: () => { },
+    run: (ed) => ed.setDevice("Tablet"),
+    stop: () => {},
   });
   Commands.add(cmdDeviceMobile, {
-    run: ed => ed.setDevice('Mobile portrait'),
-    stop: () => { },
+    run: (ed) => ed.setDevice("Mobile portrait"),
+    stop: () => {},
   });
-  Commands.add(cmdClear, (e: Editor) => confirm(txtConfirm) && e.runCommand('core:canvas-clear'));
+  Commands.add(
+    cmdClear,
+    (e: Editor) => confirm(txtConfirm) && e.runCommand("core:canvas-clear")
+  );
 
   const commands = editor.Commands;
 
-  commands.add('open-pd', {
+  commands.add("open-pd", {
     run(editor) {
-
       const pageManager = editor.Pages;
-      const panelManager = editor.Panels;
       const pages = pageManager.getAll();
-      const pagesList = document.getElementById("pages-list");
+      const pagesList = document.createElement("li");
+      pagesList.setAttribute("id", "pages-list");
+
+      pages.forEach((page) => {
+        const pageText = page.getId();
+        if (typeof pageText !== "string") return;
+        const pageItem = document.createElement("li");
+        const onClick = () => pageManager.select(pageText);
+        pageItem.addEventListener("click", onClick);
+        pageItem.innerHTML = pageText;
+        pagesList.appendChild(pageItem);
+        //now we need to add a button which can add pages her
+      });
+
+      const button = document.createElement("button");
+      button.innerHTML = "Add Page";
+      button.setAttribute("id", "pages_add");
+      button.classList.add("pages_add");
+      button.style.marginTop = "10px";
+      pagesList.appendChild(button);
 
       const lm = editor.LayerManager;
       const pn = editor.Panels;
@@ -48,30 +68,23 @@ export default (editor: Editor, config: RequiredPluginOptions) => {
       if (lmConfig.appendTo) return;
 
       if (!this.layers) {
-        const id = 'views-container';
+        const id = "views-container";
         const layers = pagesList;
         // @ts-ignore
         const panels = pn.getPanel(id) || pn.addPanel({ id });
 
-        if (lmConfig.custom) {
-          lm.__trgCustom({ container: layers });
-        } else {
-          if (layers) { layers.appendChild(lm.render()); }
-        }
-
-        panels.set('appendContent', layers).trigger('change:appendContent');
+        panels.set("appendContent", layers).trigger("change:appendContent");
         this.layers = layers;
       }
 
-      this.layers.style.display = 'block';
+      if (this.layers) {
+        this.layers.style.display = "block";
+      }
     },
 
     stop() {
       const { layers } = this;
-      layers && (layers.style.display = 'none');
+      layers && (layers.style.display = "none");
     },
-  } as CommandObject<{}, { [k: string]: any }>
-    ,
-
-  )
+  } as CommandObject<{}, { [k: string]: any }>);
 };
